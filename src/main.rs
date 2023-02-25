a0b5d48eaea5f37b9131a1ff19b61834d1a90d8b
andrew <PositiveWay05@gmail.com>
2023-02-25 23:25:19 +0500

    removed arc, mutex

--- src/main.rs
+++ src/main.rs
@@ -79,49 +79,44 @@ fn to_button(one_byte: u8) -> u16 {
 //     drop(tcp_listener);
 // }
 
-type SharedDevice = Arc<Mutex<VirtualDevice>>;
 
-fn parse_mouse(socket: UdpSocket, device: SharedDevice) {
+fn parse_mouse(socket: UdpSocket, device: &mut VirtualDevice) {
     let mut msg = [0; 2];
     loop {
         socket.recv_from(&mut msg).unwrap();
-        let mut locked_device = device.lock().unwrap();
 
         let x = to_num(msg[0]);
         let y = to_num(msg[1]);
-        locked_device.move_mouse(x, -y).unwrap();
+        device.move_mouse(x, -y).unwrap();
     }
 }
 
-fn parse_scroll(socket: UdpSocket, device: SharedDevice) {
+fn parse_scroll(socket: UdpSocket, device: &mut VirtualDevice) {
     let mut msg = [0; 1];
     loop {
         socket.recv_from(&mut msg).unwrap();
-        let mut locked_device = device.lock().unwrap();
 
         let y = to_num(msg[0]);
-        locked_device.scroll_vertical(y).unwrap();
+        device.scroll_vertical(y).unwrap();
     }
 }
 
-fn parse_button(socket: UdpSocket, device: SharedDevice) {
+fn parse_button(socket: UdpSocket, device: &mut VirtualDevice) {
     let mut msg = [0; 1];
     loop {
         socket.recv_from(&mut msg).unwrap();
-        let mut locked_device = device.lock().unwrap();
 
         if msg[0] > 128 {
             msg[0] -= 128;
-            locked_device.press(to_button(msg[0])).unwrap();
+            device.press(to_button(msg[0])).unwrap();
         } else {
-            locked_device.release(to_button(msg[0])).unwrap();
+            device.release(to_button(msg[0])).unwrap();
         }
     }
 }
 
-fn create_udp_thread(parse_func: fn(UdpSocket, SharedDevice), port: u16, device: SharedDevice) -> JoinHandle<()> {
+fn create_udp_thread(parse_func: fn(UdpSocket, &mut VirtualDevice), port: u16) -> JoinHandle<()> {
     thread::spawn(move || {
-        // let address = format!("0.0.0.0:{}", port);
         let address = "0.0.0.0";
 
         let socket = match UdpSocket::bind((address, port)) {
@@ -131,14 +126,14 @@ fn create_udp_thread(parse_func: fn(UdpSocket, SharedDevice), port: u16, device:
 
         println!("UDP at port {}:", port);
 
-        parse_func(socket, device);
+        let mut device = VirtualDevice::new();
+
+        parse_func(socket, device.borrow_mut());
     })
 }
 
 fn main() {
-    let mut device = Arc::new(Mutex::new(VirtualDevice::new()));
-
-    create_udp_thread(parse_button, 5009, device.clone());
-    create_udp_thread(parse_scroll, 5007, device.clone());
-    create_udp_thread(parse_mouse, 5005, device.clone()).join().unwrap();
+    create_udp_thread(parse_button, 5009);
+    create_udp_thread(parse_scroll, 5007);
+    create_udp_thread(parse_mouse, 5005).join().unwrap();
 }
